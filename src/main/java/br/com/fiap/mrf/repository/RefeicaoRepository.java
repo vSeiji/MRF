a2c9254c1b9bf84bf22321447a8a16c8fb47eecc
VSeiji <rm93380@fiap.com.br>
2023-05-15 21:15:34 -0300

    Security JWT - 15/05

--- src/main/java/br/com/fiap/mrf/repository/RefeicaoRepository.java
+++ src/main/java/br/com/fiap/mrf/repository/RefeicaoRepository.java
@@ -1,12 +1,8 @@
 package br.com.fiap.mrf.repository;
 
-import java.util.List;
-
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import br.com.fiap.mrf.models.Refeicao;
 
